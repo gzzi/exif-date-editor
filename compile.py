617bead96e4e67b90df2d4c93b663529e49be361
Guillaume Galeazzi <guillaume.galeazzi@gmail.com>
2020-12-31 15:10:43 +0100

    rename

--- compile.py
+++ compile.py
@@ -1,7 +1,7 @@
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
-    'main.py',
+    'exif_date_editor.py',
     #'--paths', R'C:\Program Files (x86)\Python37-32',
     '--onefile',  # fix issue with python.dll
     '--noconfirm',  # will overwrite output exe
